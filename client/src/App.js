aa95e0b7ecf0f2606a667d01cddfa39a57b962d1
Marc Majcher <majcher@gmail.com>
2021-10-01 12:42:42 -0500

    setup

---
+++ client/src/App.js
@@ -0,0 +1,27 @@
+import './App.css';
+import { useEffect, useState } from 'react';
+
+function App() {
+  const [todos, setTodos] = useState([]);
+
+  useEffect(
+    () =>
+      fetch('/todos')
+        .then((res) => res.json())
+        .then((json) => setTodos(json)),
+    []
+  );
+  return (
+    <div className="App">
+      <header className="App-header">
+        <ul>
+          {todos.map((todo) => (
+            <li key={todo.name}>{todo.name}</li>
+          ))}
+        </ul>
+      </header>
+    </div>
+  );
+}
+
+export default App;
