3f31b18dfd6c497c48c74c4854882044e4e84d11
Marc Majcher <majcher@gmail.com>
2021-10-13 16:42:25 -0500

    fresh rails

--- db/migrate/20211001172337_create_todos.rb
+++
@@ -1,8 +0,0 @@
-class CreateTodos < ActiveRecord::Migration[6.1]
-  def change
-    create_table :todos do |t|
-      t.string :name
-      t.timestamps
-    end
-  end
-end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,3 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-
-Todo.create(name: 'test one')
-Todo.create(name: 'test two')
