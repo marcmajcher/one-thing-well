1dfee4357c59e53036f49121b48da544e20ac6e6
Marc Majcher <majcher@gmail.com>
2021-10-13 19:04:00 -0500

    migrate

--- db/migrate/20211013235134_create_tasks.rb
+++ db/migrate/20211013235134_create_tasks.rb
@@ -2,14 +2,12 @@ class CreateTasks < ActiveRecord::Migration[6.1]
   def change
     create_table :tasks do |t|
       t.string :description
-      t.integer :importance
-      t.integer :urgency
-      t.boolean :completed
-      t.date :date_added
-      t.date :date_completed
+      t.integer :importance, :default => 10
+      t.integer :urgency, :default => 10
+      t.boolean :completed, :default => false
+      t.date :date_added, :default => DateTime.now
+      t.date :date_completed, :default => nil
       t.integer :user_id
-
-      t.timestamps
     end
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,5 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+spike = User.create(email: "spike@bebop.com", password_digest: "not implemented yet")
+Task.create(description: "Take Ein for a walk", user_id: spike.id)
+Task.create(description: "Get a haircut", user_id: spike.id)
+Task.create(description: "See if dinner is ready", user_id: spike.id)
+Task.create(description: "Watch tonight's Big Shot", user_id: spike.id)
